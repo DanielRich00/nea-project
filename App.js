9317eddc18848fa82d51ca4885062e00441314d3
JibranAR <113429643+JibranAR@users.noreply.github.com>
2022-09-13 23:28:00 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import './App.css';
+ 
+const App = () => {
+  return (
+    <div className="App">
+      <div>
+        
+ <h1 className="heading"> NEA PROJECT</h1>
+      
+      </div>
+    </div>
+  );
+}
+
+export default App;
